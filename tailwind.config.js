aeb13f7e2de90d00ffbe3dac428cf978951616b1
Michael <mike.taku.kane@outlook.com>
2022-12-07 20:31:27 -0700

    Changes done do Contact section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./*.{html,css,js}"],
   theme: {
     extend: {
+      boxShadow: {
+        "3xl": "0px 1px 2px rgba(0, 0, 0, 0.05)",
+      },
       fontFamily: {
         sans: "Roboto",
       },
